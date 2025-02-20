8f37648d9c0edb213d518c287060b375d24bc713
ApsaraWitharana <asachini095@gmail.com>
2025-02-20 15:04:32 +0530

    add-api gateway manage

---
+++ employee-service/src/main/java/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/all")
+    public String getEmployee(){
+        return "Employee:Sachini,Savini,Sakuni";
+    }
+
+}
